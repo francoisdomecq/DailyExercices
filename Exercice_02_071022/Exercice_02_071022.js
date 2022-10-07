4830dd43ec4017cc61ac3a32532eafb9d89d7e95
François Domecq <74706797+francoisdomecq@users.noreply.github.com>
2022-10-07 12:04:15 -0400

    Exercice_02_071022

---
+++ Exercice_02_071022/Exercice_02_071022.js
@@ -0,0 +1,32 @@
+/*
+! Longest substring without repeating characters
+
+TODO : Given a string s, find the longest substring without repeating characters. 
+TODO : s consists of English letters, digits, symbols and spaces.
+
+* Example :
+* Input: s = "abcabcbb"
+* Output: "abc"
+* Explanation: The answer is "abc", with the length of 3.
+*/
+
+var lengthOfLongestSubstring = function(s) {
+    let stringToArray = s.split("");
+    let subArray = []
+    let subArrays=[]
+    for(let i=0;i<stringToArray.length;i++){
+        if(!subArray.includes(stringToArray[i]))
+            subArray.push(stringToArray[i]);
+        else {
+            subArrays.push(subArray);
+            subArray=[stringToArray[i]];
+        }
+    }
+    if(subArray.length>0)
+        subArrays.push(subArray)
+   return subArrays.sort((a,b)=>b.length - a.length)[0].join('');
+};
+
+module.exports = {
+    lengthOfLongestSubstring
+}
---
+++ Exercice_02_071022/Exercice_02_071022.test.js
@@ -0,0 +1,15 @@
+const {lengthOfLongestSubstring} = require("./Exercice_02_071022");
+
+describe("lengthOfLongestSubstring",()=>{
+   it("should work :",()=>{
+    const samples=[
+       {input : "abcabcbb",expected : "abc"},
+       {input :"bbbbb",expected :"b"},
+       {input :"pwwkew",expected:"wke"},
+    ]
+    for(const {input,expected} of samples){
+        expect(lengthOfLongestSubstring(input)).toEqual(expected);
+       }
+   })
+ 
+})
