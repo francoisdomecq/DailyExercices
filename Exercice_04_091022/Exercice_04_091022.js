2649f89e0000a03a5a69b9777a0774bfb549c419
François Domecq <74706797+francoisdomecq@users.noreply.github.com>
2022-10-09 19:48:47 -0400

    Exercice_04_091022

---
+++ Exercice_04_091022/Exercice_04_091022.js
@@ -0,0 +1,30 @@
+/*
+! Find First and Last Position of Element in Sorted Array
+
+TODO : Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+TODO : If target is not found in the array, return [-1, -1].
+TODO : You must write an algorithm with O(log n) runtime complexity.
+
+* Examples :
+*
+* Input: nums = [5,7,7,8,8,10], target = 8
+* Output: [3,4]
+*
+* Input: nums = [5,7,7,8,8,10], target = 6
+* Output: [-1,-1]
+*/
+
+var searchRange = function(nums, target) {
+    const index=[];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===target){
+            index.push(i);
+        }
+    }
+    if(index.length>0)
+        return [index[0],index[index.length-1]];
+    else
+        return [-1,-1];
+};
+
+module.exports={searchRange}
---
+++ Exercice_04_091022/Exercice_04_091022.test.js
@@ -0,0 +1,15 @@
+var {searchRange} = require('./Exercice_04_091022');
+
+describe("searchRange",()=>{
+    it("should work : ",()=>{
+        const samples =[
+            { nums : [5,7,7,8,8,10], target : 8, expected : [3,4]},
+            { nums : [], target : 0, expected : [-1,-1]},
+            { nums : [5,7,7,8,8,10], target : 6, expected : [-1,-1]}
+        ]
+        for(const {nums,target,expected} of samples){
+            expect(searchRange(nums,target)).toEqual(expected);
+        }
+
+    })
+})
