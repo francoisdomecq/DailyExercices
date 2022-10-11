eced19077fbfbd7203cffe1db1c2763c8ace2947
François Domecq <74706797+francoisdomecq@users.noreply.github.com>
2022-10-10 20:56:26 -0400

    Exercice_05_101022

---
+++ Exercice_05_101022/Exercice_05_101022.js
@@ -0,0 +1,30 @@
+/*
+! GetSubArrayNumber
+
+TODO Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+* Examples:
+* [1,1,1] et k=2 => output = 2
+* [1,2,3,0,3] et k=3 => output = 5
+* [4, 2, 9, 7, 19] et k=5 => output = 0
+* [1, -1, 2] et k=2 ⇒ output = 2
+*/
+
+function getSubArrayNumber(arr, k) {
+  let numbersOfArray = 0;
+  let sum = 0;
+  for(let i=0;i<arr.length;i++){
+    sum=0;
+    for(let j=i;j<arr.length;j++){
+      sum+=arr[j];
+      if(sum===k){
+        numbersOfArray++;
+      }
+    }
+  }
+  return numbersOfArray;
+}
+
+module.exports = {
+  getSubArrayNumber,
+};
---
+++ Exercice_05_101022/Exercice_05_101022.test.js
@@ -0,0 +1,16 @@
+const { getSubArrayNumber } = require("./Exercice_05_101022");
+
+describe("getSubArrayNumber", () => {
+  it("should work :)", () => {
+    const samples = [
+      { array: [1, 1, 1], k: 2, expected: 2 },
+      { array: [1, 2, 3, 0, 3], k: 3, expected: 5 },
+      { array: [4, 2, 9, 7, 19], k: 5, expected: 0 },
+      { array: [1, -1, 2], k: 2, expected: 2 },
+    ];
+
+    for (const { array, k, expected } of samples) {
+      expect(getSubArrayNumber(array, k)).toBe(expected);
+    }
+  });
+});
