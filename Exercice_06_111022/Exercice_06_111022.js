1db7b60ff54db3d7f68d6cfecc2d6b0eed61d207
François Domecq <74706797+francoisdomecq@users.noreply.github.com>
2022-10-12 19:42:10 -0400

    Exercice_06_111022

--- Exercice_06_111022/Exercice_06_111022.js
+++ Exercice_06_111022/Exercice_06_111022.js
@@ -1,3 +1,20 @@
+/*
+! Palindrome Number
+
+TODO : Given an integer x, return true if x is palindrome integer.
+
+? An integer is a palindrome when it reads the same backward as forward.
+
+* Examples : 
+* Input: x = 121
+* Output: true
+* Explanation: 121 reads as 121 from left to right and from right to left. 
+
+* Input: x = -121
+* Output: false
+* Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+*/
+
 var isPalindrome = function(x) {
     let isPalindromeBoolean = true;
     if(x<0)
