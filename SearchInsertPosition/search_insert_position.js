4347d960d4550683abc99fd65f6e4de484e31f31
francoisdomecq <fradomecq@gmail.com>
2022-11-16 20:48:37 -0500

    Description

--- SearchInsertPosition/search_insert_position.js
+++ SearchInsertPosition/search_insert_position.js
@@ -1,27 +1,33 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
- var searchInsert = function(nums, target) {
-    if(target<nums[0]){
-        return 0
-    }
-    else if(target>nums[nums.length-1]){
-        return nums.length
-    }
-    else{
-        for(let i=0;i<=nums.length-1;i++){
-         if(nums[i]===target){
-            return i
-        }
-        else if(nums[i]<target && nums[i+1]>target){
-            return i+1;
-        }
-    }
-    }
-};
+/*
+! Search Insert Position 
+TODO : Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+TODO : If not, return the index where it would be if it were inserted in order.
+TODO : You must write an algorithm with O(log n) runtime complexity.
 
-module.exports ={
-    searchInsert
+* Examples : 
+* Input: nums = [1,3,5,6], target = 5
+* Output: 2
+
+* Input: nums = [1,3,5,6], target = 2
+* Output: 1
+*/
+
+var searchInsert = function (nums, target) {
+    if (target < nums[0]) {
+        return 0
+    } else if (target > nums[nums.length - 1]) {
+        return nums.length
+    } else {
+        for (let i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] === target) {
+                return i
+            } else if (nums[i] < target && nums[i + 1] > target) {
+                return i + 1
+            }
+        }
+    }
+}
+
+module.exports = {
+    searchInsert,
 }
